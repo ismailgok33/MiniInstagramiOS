7483478cb17198658a16d8eb15cbc2fb3f560274
İsmail <ismailgok33@gmail.com>
2022-03-09 18:44:04 -0500

    Initial Commit

---
+++ MiniInstagram/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MiniInstagram
+//
+//  Created by İsmail on 9.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MiniInstagram/MiniInstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniInstagramApp.swift
+//  MiniInstagram
+//
+//  Created by İsmail on 9.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct MiniInstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
