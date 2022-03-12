9b9e58599ea5bace6b35eb79810ff4bd0fceec35
İsmail <ismailgok33@gmail.com>
2022-03-12 01:28:11 -0500

    Google Login is added

---
+++ MiniInstagram/View/Authentication/EmailLoginView.swift
@@ -0,0 +1,20 @@
+//
+//  EmailLoginView.swift
+//  MiniInstagram
+//
+//  Created by İsmail on 12.03.2022.
+//
+
+import SwiftUI
+
+struct EmailLoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EmailLoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmailLoginView()
+    }
+}
